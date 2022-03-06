6006fb0b6e3d46bfed11ee06c5d5bb5d2ecb2cfe
bashirazizov <bashir.azizov@code.edu.az>
2022-03-06 20:47:58 +0400

    init

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 04-mar
+            //Console.WriteLine("Imtahan Balinizi daxil edin: ");
+            //int ExamScore = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Giris Balinizi daxil edin: ");
+            //int EntryScore = Convert.ToInt32(Console.ReadLine());
+
+            //int FinalScore = ExamScore + EntryScore;
+
+            //if (ExamScore >= 0 && ExamScore <= 50 && EntryScore >= 0 && EntryScore <= 50)
+            //{
+            //    if (ExamScore < 17 || FinalScore <= 50)
+            //    {
+            //        Console.WriteLine("Failed.");
+            //    }else if (FinalScore <= 60)
+            //    {
+            //        Console.WriteLine("E");
+            //    }else if (FinalScore <= 70)
+            //    {
+            //        Console.WriteLine("D");
+            //    }else if (FinalScore <= 80)
+            //    {
+            //        Console.WriteLine("C");
+            //    }else if (FinalScore <= 90)
+            //    {
+            //        Console.WriteLine("B");
+            //    }else
+            //    {
+            //        Console.WriteLine("A");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Input.");
+            //}
+
+            //Console.WriteLine("End");
+
+
+            //Console.WriteLine("Ededi daxil edin: ");
+            //int Number = Convert.ToInt32(Console.ReadLine());
+            //if (Number >= 10 && Number < 100)
+            //{
+            //    int Ones = Number % 10;
+            //    int Tens = (Number - Ones) / 10;
+
+            //    if (Ones == Tens)
+            //    {
+            //        Console.WriteLine("Equal");
+            //    }else if (Ones > Tens)
+            //    {
+            //        Console.WriteLine("Teklikler Boyukdur");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Onluqlar Boyukdur");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
+
+            //int a = 5;
+            //int b = 2;
+            //int c = 11;
+            //int d = 24;
+            //int e = 72;
+            //int g = 37;
+
+
+            //if (a > b)
+            //{
+            //    Console.WriteLine("Point1");
+            //    if (d > c)
+            //    {
+            //        Console.WriteLine("Point2");
+            //    }
+            //}
+            //else
+            //{
+            //}
+
+
+            //if (a > b)
+            //{
+            //    Console.WriteLine("Point3");
+            //} 
+            //else if(d > c)
+            //{
+            //    Console.WriteLine("Point4");
+            //}
+            //else
+            //{
+            //}
+
+            //Console.WriteLine("Heftenin gununu daxil edin: ");
+            //int Day = Convert.ToInt32(Console.ReadLine());
+
+
+            //if (Day == 1)
+            //{
+            //    Console.WriteLine("Bazar ertesi");
+            //}else if(Day == 2)
+            //{
+            //    Console.WriteLine("C a");
+            //}else if (Day == 3)
+            //{
+            //    Console.WriteLine("C");
+            //}
+            //else if (Day == 4)
+            //{
+            //    Console.WriteLine("Cume a");
+            //}
+            //else if (Day == 5)
+            //{
+            //    Console.WriteLine("Cume");
+            //}
+            //else if (Day == 6)
+            //{
+            //    Console.WriteLine("Sembe");
+            //} else if (Day == 7)
+            //{
+            //    Console.WriteLine("Bazar");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid in");
+            //}
+
+
+            //switch (Day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("bzr ert");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("crsnb ax");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("crsnb");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("cm ax");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("cm");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("smb");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("bzr");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid in");
+            //        break;
+            //}
+
+            //int n1 = 10;
+            //int n2 = 13;
+            //int n3 = 19;
+            //int n4 = 52;
+            //int n5 = 81;
+
+            //int result = (n1 + n2 + n3 + n4+ n5) / 5;
+
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int i = 0;
+            //while (i<10)
+            //{
+            //    Console.WriteLine("salam");
+            //}
+
+            //int i = 0;
+
+            //for (; ;)
+            //{
+            //    i++;
+            //    Console.WriteLine(i);
+            //    if (i>100)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //int Result = 0;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Result += i;
+            //    Console.WriteLine(Result);
+            //}
+
+            //Console.WriteLine("Neice: "+Result.ToString());
+
+            //int i = 6;
+
+            //do
+            //{
+            //    Console.WriteLine("Point1");
+            //    i++;
+            //} while (10>i);
+
+            //while (5 > 6)
+            //{
+            //    Console.WriteLine("Point2");
+            //}
+            #endregion
+
+            //string[] names = { "mamed", "isabala", "rasim", "elman", "azim", "sakir" };
+
+            //Console.WriteLine(names[names.Length-1]);
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(i + " - " + names[i]);
+            //}
+
+            //int[] oneDimensionalNums = { 1, 2, 3, 4, 5, 6, 7 };
+            //int[,] twoDimensionalNums = { { 1, 2, 12 }, { 3, 4, 43 }, { 5, 6, 51 }, { 5, 6, 52 }, { 5, 6, 73 } };
+
+            //int[,,] threeDimensionalNums = { { { 1, 2 }, { 3, 4 } }, { { 5, 6 }, { 7, 8 } } };
+
+            //Console.WriteLine(oneDimensionalNums[1]);
+            //Console.WriteLine(twoDimensionalNums[0,0]);
+            //Console.WriteLine(threeDimensionalNums[1,1,0]);
+
+            //for (int i = 0; i < oneDimensionalNums.Length; i++)
+            //{
+            //    Console.WriteLine(oneDimensionalNums[i]);
+            //}
+
+            //Console.WriteLine(twoDimensionalNums.Length);
+
+            //Console.WriteLine(twoDimensionalNums.Length);
+            //Console.WriteLine(twoDimensionalNums.GetLength(0));
+            //Console.WriteLine(twoDimensionalNums.GetLength(1));
+
+
+            //for (int i = 0; i < twoDimensionalNums.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < twoDimensionalNums.GetLength(1); j++)
+            //    {
+            //        Console.Write(twoDimensionalNums[i, j]+" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //var person = new
+            //{
+            //    name = "rasim",
+            //    surname = "isayev",
+            //    age = 21,
+            //    isMarried = false
+            //};
+
+            //Console.WriteLine(person.name);
+
+
+            //Console.WriteLine(nums[0]);
+
+            //string[] names = { "mamed", "isabala", "rasim", "elman", "azim", "sakir" };
+
+            //int loopCount = 0;
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    loopCount++;
+            //    if (names[i] == "sakir")
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine(loopCount);
+
+            //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //for (int i = nums.Length-1; i >= 0 ; i--)
+            //{
+            //    for (int j = 0; j < nums.Length; j++)
+            //    {
+            //        Console.WriteLine(nums[i]+ nums[j]);
+            //    }
+            //}
+
+            //Console.WriteLine("asagi reqemi daxil edin: ");
+            //int lowNum = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("yuxari reqemi daxil edin: ");
+            //int highNum = Convert.ToInt32(Console.ReadLine());
+
+            //int result = 0;
+            //int loopCount = 0;
+
+            //int start = 0;
+            //if (lowNum % 2 == 0)
+            //{
+            //    start = lowNum + 2;
+            //}
+            //else
+            //{
+            //    start = lowNum + 1;
+            //}
+
+            //for (int i = start; i < highNum; i += 2)
+            //{
+            //    loopCount++;
+            //    result += i;
+            //}
+            //Console.WriteLine(result);
+
+            //Console.WriteLine("LoopCount: " + loopCount);
+
+            Console.WriteLine("reqemi daxil edin: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            bool isSimple = true;
+            int loopCount = 0;
+
+            for (int i = 2; i * i < num; i++)
+            {
+                loopCount++;
+                if (num%i == 0)
+                {
+                    isSimple = false;
+                }
+            }
+
+            Console.WriteLine(isSimple);
+            Console.WriteLine(loopCount);
+        }
+    }
+}
